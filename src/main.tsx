fd87ade7f288a6b7b9929aa93181e08bce2ecd33
agrippa <afsalahamed07@gmail.com>
2024-11-08 22:33:50 +0530

    some router warning saying me to opt to v7

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,18 @@ import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import routes from "./routes";
 
-const router = createBrowserRouter(routes);
+const router = createBrowserRouter(routes, {
+  future: {
+    v7_fetcherPersist: true,
+    v7_relativeSplatPath: true,
+    v7_normalizeFormMethod: true,
+    v7_partialHydration: true,
+    v7_skipActionErrorRevalidation: true,
+  },
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <RouterProvider router={router} future={{ v7_startTransition: true }} />
   </StrictMode>,
 );
