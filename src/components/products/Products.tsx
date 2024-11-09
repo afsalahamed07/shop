e804ab1761ee98d0cfa479af7f3e59761c946f32
agrippa <afsalahamed07@gmail.com>
2024-11-09 20:25:42 +0530

    dynamic products per page

--- src/components/products/Products.tsx
+++ src/components/products/Products.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import productsList from "../../assets/products.json";
 import Product from "./Product";
 import Card from "./Card";
@@ -6,8 +6,9 @@ import "./Products.css";
 
 const Products: React.FC = () => {
   const [page, setPage] = useState<number>(0);
+  const [productsPerPage, setProductsPerPage] = useState<number>(2);
   const products: Product[] = Object.values(productsList);
-  const pages: number = Math.ceil(products.length / 3);
+  const pages: number = Math.ceil(products.length / productsPerPage);
 
   function previouse() {
     if (page != 0) setPage((prev) => prev - 1);
@@ -17,21 +18,43 @@ const Products: React.FC = () => {
     if (page < pages - 1) setPage((prev) => prev + 1);
   }
 
+  function handleResize() {
+    if (window.innerHeight >= 1300) {
+      setProductsPerPage(4);
+    } else if (window.innerHeight >= 900) {
+      setProductsPerPage(3);
+    } else {
+      setProductsPerPage(2);
+    }
+  }
+
+  useEffect(() => {
+    handleResize();
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
     <div className="products-parent">
-      <div className="products">
-        {products.slice(page * 3, page * 3 + 3).map((product) => (
-          <Card
-            key={product.id}
-            id={product.id}
-            title={product.title}
-            price={product.price}
-            category={product.category}
-            rating={product.rating}
-            image={product.image}
-            description={product.description}
-          />
-        ))}
+      <div className={`products grid-rows-${productsPerPage}`}>
+        {products
+          .slice(
+            page * productsPerPage,
+            page * productsPerPage + productsPerPage,
+          )
+          .map((product) => (
+            <Card
+              key={product.id}
+              id={product.id}
+              title={product.title}
+              price={product.price}
+              category={product.category}
+              rating={product.rating}
+              image={product.image}
+              description={product.description}
+            />
+          ))}
       </div>
       <div className="page">
         <div className="btn" onClick={previouse}>
